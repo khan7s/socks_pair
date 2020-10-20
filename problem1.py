a9efcce54a97e5a2bdcfb1819bcd6dcdb2d190cc
khan7s <40593474+khan7s@users.noreply.github.com>
2020-10-20 15:37:31 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,22 @@
+def pile():
+
+    n = int(input("Enter the no. of socks in pile: "))
+
+    a = []
+    for i in range(0,n):
+        a.append(int(input()))
+    a.sort()
+#checking the pairs of socks with same colors
+    count = 0
+    i=0
+    for i in range(n+1):
+        try:
+            if a[i]==a[i+1]:
+                a.pop(i)
+                a.pop(i+1)
+                count+=1
+
+        except:
+            pass
+    print(count)
+pile()
